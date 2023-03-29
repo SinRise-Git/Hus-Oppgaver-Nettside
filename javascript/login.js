681c2da5c5bab756a80bd899bb7311e2941f7a4a
SineRiseX <bfu01.bb@gmail.com>
2023-03-29 20:42:45 +0100

    update

---
+++ javascript/login.js
@@ -0,0 +1,23 @@
+function showLoginForm(){
+    document.getElementById("login-form").style.display = "block";
+    document.getElementById("signup-form").style.display = "none";
+    document.title = "Login";
+    // Get the link element
+    const linkElement = document.querySelector('link[rel="icon"]');
+
+    // Change the href attribute to the new image path
+    linkElement.href = '/images/enter.png';
+}
+
+function showSignupForm(){
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("signup-form").style.display = "block";
+    document.title = "Signup";
+     // Get the link element
+     const linkElement = document.querySelector('link[rel="icon"]');
+
+     // Change the href attribute to the new image path
+     linkElement.href = '/images/add-user.png';
+    
+     
+}
