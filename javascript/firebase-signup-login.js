15f7b4a2e1fe583dfc3d9f2bd8199a11275a865d
BrageDrage909 <bfu01.bb@gmail.com>
2023-04-20 13:07:31 +0200

    update

--- javascript/firebase-signup-login.js
+++ javascript/firebase-signup-login.js
@@ -5,7 +5,7 @@ import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasej
 const auth = getAuth(app);
 const database = getDatabase(app);
 
-
+//sign up form
 submitData.addEventListener('click', async (e) => {
 
     var email = document.getElementById('gmail-signup').value;
@@ -23,7 +23,7 @@ submitData.addEventListener('click', async (e) => {
         .then((userCredential) => {
             // Signed in
             const user = userCredential.user;
-            // ... user.uid
+            
             set(ref(database, 'users/' + user.uid), {
                 email: email,
                 password: password,
@@ -48,15 +48,48 @@ submitData.addEventListener('click', async (e) => {
         })
         .catch((error) => {
             const errorMessage = error.message;
-            console.log(errorMessage);
             if (errorMessage === "Firebase: Password should be at least 6 characters (auth/weak-password).") {
               document.getElementById("error-message").innerHTML = "Error: Password should be at least 6 characters";
             } else if (errorMessage === "Firebase: Error (auth/invalid-email).") {
               document.getElementById("error-message").innerHTML = "Error: Not a valid email address";
             } else if (errorMessage === "Firebase: Error (auth/email-already-in-use).") {
-            document.getElementById("error-message").innerHTML = "Error: Email already in use";
+              document.getElementById("error-message").innerHTML = "Error: Email already in use";
+            } else if (errorMessage === "Firebase: Error (auth/internal-error)."){
+              document.getElementById("error-message").innerHTML = "Error: Auto/internal-error";
             } else {
               document.getElementById("error-message").innerHTML = errorMessage;
             }
           });
+});
+
+//login form
+loginData.addEventListener('click', async (e) => {
+      
+  var GmailLogin = document.getElementById('username-login').value;
+  var passwordLogin = document.getElementById('password-login').value;
+
+
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, GmailLogin, passwordLogin)
+     .then((userCredential) => {
+     // Signed in 
+       const user = userCredential.user;
+       
+  })
+     .catch((error) => {
+      const errorMessageLogin = error.message;
+      if (errorMessageLogin === "Firebase: Error (auth/wrong-password).") {
+        document.getElementById("error-message-login").innerHTML = "Error: Wrong password.";
+      } else if (errorMessageLogin === "Firebase: Error (auth/invalid-email).") {
+        document.getElementById("error-message-login").innerHTML = "Error: Invalid email address.";
+      } else if (errorMessageLogin === "Firebase: Error (auth/user-not-found).") {
+        document.getElementById("error-message-login").innerHTML = "Error: User not found for this email.";
+      } else if (errorMessageLogin === "Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).") {
+        document.getElementById("error-message-login").innerHTML = "Error: Too many failed attempts. Please try again later.";
+      } else {
+        document.getElementById("error-message").innerHTML = errorMessage;
+      }
+       
+  });
+
 });
--- javascript/form-change.js
+++ javascript/form-change.js
@@ -2,20 +2,18 @@ function showLoginForm(){
     document.getElementById("login-form").style.display = "block";
     document.getElementById("signup-form").style.display = "none";
     document.title = "Login";
-    // Get the link element
     const linkElement = document.querySelector('link[rel="icon"]');
-
-    // Change the href attribute to the new image path
     linkElement.href = '/images/enter.png';
+    document.getElementById("form-box").style.height = "450px"; 
 }
 
 function showSignupForm(){
     document.getElementById("login-form").style.display = "none";
     document.getElementById("signup-form").style.display = "block";
     document.title = "Signup";
-     // Get the link element
-     const linkElement = document.querySelector('link[rel="icon"]');
-
-     // Change the href attribute to the new image path
-     linkElement.href = '/images/add-user.png';
+    const linkElement = document.querySelector('link[rel="icon"]');
+    linkElement.href = '/images/add-user.png';
+    document.getElementById("form-box").style.height = "550px"; 
 }
+
+
