7a139d138ea8db3061b42bf50102e97f68586578
BrageDrage909 <bfu01.bb@gmail.com>
2023-04-24 12:35:35 +0200

    update

--- javascript/form-change.js
+++ javascript/form-change.js
@@ -1,18 +1,18 @@
 function showLoginForm(){
+    const linkElement = document.querySelector('link[rel="icon"]');
+    linkElement.href = '/images/enter.png';
     document.getElementById("login-form").style.display = "block";
     document.getElementById("signup-form").style.display = "none";
     document.title = "Login";
-    const linkElement = document.querySelector('link[rel="icon"]');
-    linkElement.href = '/images/enter.png';
     document.getElementById("form-box").style.height = "450px"; 
 }
 
 function showSignupForm(){
+    const linkElement = document.querySelector('link[rel="icon"]');
+    linkElement.href = '/images/add-user.png';
     document.getElementById("login-form").style.display = "none";
     document.getElementById("signup-form").style.display = "block";
     document.title = "Signup";
-    const linkElement = document.querySelector('link[rel="icon"]');
-    linkElement.href = '/images/add-user.png';
     document.getElementById("form-box").style.height = "550px"; 
 }
 
--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,25 @@
 import { app } from "/javascript/firebaseconfig.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js";
 
+const db = getDatabase(app);
+const uid = localStorage.getItem("uid");
+const userRef = ref(db, `users/${uid}`);
+
+if (!localStorage.getItem("username")){
+  onValue(userRef, (snapshot) => {
+    const userData = snapshot.val();
+    console.log("userData", userData);
+    localStorage.setItem('username',  userData.username);
+    document.querySelector(".username").innerHTML = localStorage.getItem("username");
+  });
+} else { 
+  document.querySelector(".username").innerHTML = localStorage.getItem("username");
+}
 
 logoutbutton.addEventListener('click', async (e) => {
     window.location.href = 'login-signup.html';
     localStorage.removeItem("uid");
     localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("username");
 });
 
